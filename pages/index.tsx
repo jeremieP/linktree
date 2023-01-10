2085a99b121023871914a8d247338c9f233fabb7
jeremieP <jeremie.pereira@gmail.com>
2023-01-10 23:00:55 +0100

    feat: Initial commit (Next / Tailwind base)

---
+++ pages/index.tsx
@@ -0,0 +1,17 @@
+import Head from 'next/head'
+// import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Jérémie Pereira</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <h1 className="text-3xl font-bold underline">
+        Hello world!
+      </h1>
+    </>
+  )
+}
