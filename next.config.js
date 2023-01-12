bc6404bac9b59cef0451bcb851027a9cd628afb8
jeremieP <jeremie.pereira@gmail.com>
2023-01-12 01:05:12 +0100

    feat: add 404 page

--- next.config.js
+++ next.config.js
@@ -3,7 +3,12 @@ const nextConfig = {
   reactStrictMode: true,
   images: {
     domains: ['pbs.twimg.com', 'static-cdn.jtvnw.net'],
-  }
+  },
+  experimental: {
+    fontLoaders: [
+      { loader: '@next/font/google', options: { subsets: ['latin'] } },
+    ],
+  },
 }
 
 module.exports = nextConfig
---
+++ src/components/DeadLink/index.tsx
@@ -0,0 +1,5 @@
+import styles from './index.module.css'
+
+export const DeadLink = () => (
+  <div className={styles.deadLink} aria-hidden></div>
+)
