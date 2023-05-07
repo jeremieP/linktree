28752eb3563d6cda2d551c198b9833cd4d922433
jeremieP <jeremie.pereira@gmail.com>
2023-05-07 15:22:04 +0200

    feat: init RootLayout

---
+++ app/layout.tsx
@@ -0,0 +1,13 @@
+interface RootLayoutProps {
+  children: React.ReactNode
+}
+
+export default function RootLayout({
+  children,
+}: RootLayoutProps) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  )
+}
